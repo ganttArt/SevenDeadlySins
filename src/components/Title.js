411118714174ac1a4b6be5b45a048b4b110db2ee
Chris Gantt <gantt.art@gmail.com>
2020-03-03 16:49:03 -0800

    Title component added and initialized. Quotes.js and Sins.js files added to components directory for future development

---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Title extends Component {
+  render() {
+    return (
+      <div className="title">
+        <h1 className="welcome">Welcome to Hell</h1>
+        <h3 >Please explore these seven deadly sins</h3>
+        <p>One of them probably brought you here...</p>
+      </div>
+    );
+  }
+}
+
+export default Title;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import Title from './components/Title';
+import "./style.css";
 
-import App from "./App";
+class App extends Component {
+  render() {
+    return (
+      <div className="app-container">
+      <Title/>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
