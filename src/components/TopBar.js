d1d06d7a44d174d31ba6eb2679bb817e2e55816b
Chris Gantt <gantt.art@gmail.com>
2020-03-25 14:29:58 -0700

    Setup basic css for TopBar component.

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import pentagram from './assets/hell/pentagram.jpg'
 
 class TopBar extends Component{
     render() {
         return (
-          <div className="top-bar">
+          <header className="top-bar">
+            {/* <img src={pentagram} alt="logo"/> */}
             <h1 className="top-bar-logo">H</h1>
             <h1 className="top-bar-title">Welcome to Hell</h1>
             <button className="feeling-naughty-button">Feeling Nice?</button>
-          </div>
+          </header>
         );
     }
 }
