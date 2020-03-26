3469a77f1c2d9530ca9547e870fa93d52d295808
Chris Gantt <gantt.art@gmail.com>
2020-03-26 12:58:49 -0700

    Hell logo created and alert implemented onClick. Image assets moved up one directory to src.

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import pentagram from './assets/hell/pentagram.jpg'
 
 class TopBar extends Component{
     render() {
         return (
           <header className="top-bar">
-            {/* <img src={pentagram} alt="logo"/> */}
-            <h1 className="top-bar-logo">H</h1>
+            <button
+              className="top-bar-logo-button"
+              onClick = {() => alert("Congratulations!\nYou found the hidden button!\nAnd because you're in hell, your prize is eternal suffering.\nThanks for visiting!")}  
+            />
             <h1 className="top-bar-title">Welcome to Hell</h1>
             <button className="feeling-naughty-button">Feeling Nice?</button>
           </header>
