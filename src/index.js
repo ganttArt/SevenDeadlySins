7cd67694501c467f918c69db4d55cafe968ea3b4
Chris Gantt <gantt.art@gmail.com>
2020-03-27 16:52:16 -0700

    Implemented responsive design from full-screen laptop viewing to phone display sizes.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import QuoteState from './components/QuoteSinsContainer'
-
 import LeftSideBar from './components/LeftSideBar'
 import TopBar from './components/TopBar'
 
 import "./style.css";
+import "./responsive.css";
 
 class App extends Component {
   render() {
