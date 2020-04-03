d16a236388bede785fdccacf38ed21d6a9b92ed5
Chris Gantt <gantt.art@gmail.com>
2020-04-03 13:51:59 -0700

    Remove copywright symbol from Credits component.

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -4,7 +4,6 @@ class Credits extends Component{
     render() {
         return (
           <div className="credits-container">
-            <button id='copywright-symbol'/>
             <p id='name'>Chris Gantt - 2020</p>
             <div id='links'>
               <a id='linkedin' href='https://www.linkedin.com/in/gantt/'>LinkedIn</a>
